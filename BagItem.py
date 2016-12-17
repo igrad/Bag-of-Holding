33f1bad4234c569da192c4ff932ed4a9d8ef5f06
Ian Gradert <iangradert@gmail.com>
2016-12-17 17:23:46 -0600

    Fixed UpdateItem method to allow multiple property changes in one save-file push.

--- BagItem.py
+++ BagItem.py
@@ -1,8 +1,6 @@
 from SysFuncs import *
 from SaveStores import *
 
-ITEMIDCTR = 0
-
 class BagItem():
     '''An unassigned item that represents all the item-specific data required by the user.
     ----------
@@ -11,13 +9,22 @@ class BagItem():
     String desc: A brief description of the item
     String icon: File path to the icon image
     List(String) tags: List of tags that describe this item'''
+    ID = 0
+    name = 'item'
+    qty = 1
+    weight = 1.00
+    val = 1
+    desc = 'description'
+    icon = 'images/none.png'
+    tags = []
 
     def __init__(self, **kwargs):
         if 'ID' in kwargs: self.ID = int(kwargs['ID'])
         else: self.ID = GetItemIndex()
         if 'name' in kwargs: self.name = str(kwargs['name'])
         if 'qty' in kwargs: self.qty = int(kwargs['qty'])
-        if 'mass' in kwargs: self.mass = float(kwargs['qty'])
+        if 'weight' in kwargs: self.weight = float(kwargs['qty'])
+        if 'val' in kwargs: self.val = float(kwargs['val'])
         if 'desc' in kwargs: self.desc = str(kwargs['desc'])
         if 'icon' in kwargs: self.icon = str(kwargs['icon'])
         if 'tags' in kwargs: self.icon = list(kwargs['tags'])
@@ -35,9 +42,10 @@ class BagItem():
     def UpdateItem(self, **kwargs):
         if 'name' in kwargs: self.name = str(kwargs['name'])
         if 'qty' in kwargs: self.qty = int(kwargs['qty'])
-        if 'mass' in kwargs: self.mass = float(kwargs['qty'])
+        if 'weight' in kwargs: self.weight = float(kwargs['qty'])
+        if 'val' in kwargs: self.val = float(kwargs['val'])
         if 'desc' in kwargs: self.desc = str(kwargs['desc'])
         if 'icon' in kwargs: self.icon = str(kwargs['icon'])
         if 'tags' in kwargs: self.icon = list(kwargs['tags'])
 
-        UpdateItemInfo(self.ID)
+        UpdateItemInfo(self.ID, self.qty, self.weight, self.val, self.desc, self.icon, self.tags)
