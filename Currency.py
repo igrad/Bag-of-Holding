86b75bd58e54c36aa047d3496efe020c373827c5
Ian Gradert <iangradert@gmail.com>
2016-12-17 16:28:10 -0600

    Initial setup for currency system being.
    Currency systems are designated on a bag-to-bag basis. Once bag can have multiple forms of currencies (dollars, gold, euros, silver, etc), so long as they are equated to a unit value.

---
+++ Currency.py
@@ -0,0 +1,27 @@
+from SysFuncs import *
+
+CurrencyUnit = namedtuple('CurrencyUnit', 'name unitval')
+
+class CurrencySet():
+    cTypes = list()
+
+    def __init__(self, **kwargs):
+        if 'cTypes' in kwargs:
+            self.cTypes = sorted([CurrencyUnit(x[0], x[1]) for x in kwargs['cTypes']], key = lambda x:x[1])
+
+    def GetCNames(self):
+        return self.cTypes.keys()
+
+    def GetCVals(self):
+        return self.cTypes.values()
+
+    def AddNewCType(self, name, unitval):
+        newCType = CurrencyUnit(name, unitval)
+
+        self.cTypes.append(newCType)
+        self.cTypes = sorted(cTypes, key = lambda x:x[1])
+
+    def RemoveCType(self, name):
+        for cType in self.cTypes:
+            if cType.name == name:
+                del self.cTypes[cType]
