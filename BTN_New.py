c05d6fe9f19b0092dfe82074b379d13664654e4a
Ian Gradert <iangradert@gmail.com>
2016-12-19 22:08:42 -0600

    Initial method to pull contpane_new onto into focus.

--- BTN_New.py
+++ BTN_New.py
@@ -2,11 +2,18 @@ from SysFuncs import *
 from AppInit import *
 from Bag import *
 from BagItem import *
-from ItemView import *
-from SaveStores import *
 
 # This file contains all of the button-bound functions relating to the creation of a new
 # BagItem through the New tab on the main screen of the app.
 
-def OpenNewItemScreen():
-    
+def OpenContPane_New(obj):
+    new_name.text = ''
+    new_icon.source = 'images/blankIcon.png'
+    new_qty.text = ''
+    new_weight.text = ''
+    new_val.text = ''
+    new_tags.text = ''
+    new_desc.text = ''
+
+    contpane_items.pos = CONT_POS_L
+    contpane_new.pos = CONT_POS
