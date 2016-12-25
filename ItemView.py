6f6c67e5d3a89f4a89c49c46fe2e4a7a4c8aa34b
Ian Gradert <iangradert@gmail.com>
2016-12-25 16:22:56 -0600

    Starting work on correcting initial ItemView setup
    - Changed from Button subclass to a RelativeLayout subclass that implies the ButtonBehavior interface
    - Had to manually set the width of the name label to fit the hinted size, which was not compiling as expected during initial tests. This also applies the 'scale' variable to correct the width, and set the Label to left-align the text contents. This may cause problems in the future (though I don't expect anything immediate), which is why the new branch was started, just for testing purposes.

--- ItemView.py
+++ ItemView.py
@@ -3,36 +3,45 @@ from BagItem import *
 from LoadSaves import *
 from AppInit import *
 
-IV_ICON = SizeMap(175, 175, LISTITEM.sizePair)
-IV_NAME = SizeMap(800, 75, LISTITEM.sizePair)
+IV_ICON = SizeMap(172, 172, LISTITEM.sizePair)
+IV_NAME = SizeMap(790, 75, LISTITEM.sizePair)
 IV_DESC = SizeMap(800, 65, LISTITEM.sizePair)
 
-IV_ICON_POS = (5 / scale, 5 / scale)
+IV_ICON_POS = (14 / scale, 14 / scale)
+IV_NAME_POS = (218 / scale, 102 / scale)
 
-class ItemView(Button):
+class ItemView(ButtonBehavior, RelativeLayout):
     view = RelativeLayout(size_hint = FILLS)
     itemID = 0
-    dicon = Image(size_hint = IV_ICON.hSizePair, source = 'images/none.png',
-                  pos = IV_ICON_POS, allow_stretch = True, keep_ratio = False)
-    dname = Label(text = 'Item Name', font_name = FONT_BASK, font_size = FONT_SIZE_A,
-                  color = [0,0,0,1], size = IV_NAME.sizePair)
+
+    dBG = Image(size_hint = FILLS, source = 'images/IMG_ITEMVIEWBG.png',
+        allow_stretch = True, keep_ratio = False)
+    dicon = Image(size_hint = IV_ICON.hSizePair, pos = IV_ICON_POS,
+        source = 'images/none.png', allow_stretch = True, keep_ratio = False)
+    dname = Label(size_hint = IV_NAME.hSizePair, pos = IV_NAME_POS, halign = 'left',
+        valign = 'middle', text = 'Item Name', font_name = FONT_BASK,
+        font_size = FONT_SIZE_A, color = [1,1,1,1], shorten = True,
+        shorten_from = 'right')
 
     def __init__(self, itemID, **kwargs):
         try:
             self.itemID = itemID
 
-            super(ItemView, self).__init__(background_down = 'images/IMG_ITEMVIEWBG.png',
-                background_normal = 'images/IMG_ITEMVIEWBG.png',
-                size_hint = FILLS, **kwargs)
+            super(ItemView, self).__init__(size_hint = FILLS, **kwargs)
 
-            print("TROUBLESHOOTING || This Item's ID: " + str(self.itemID))
-            print("TROUBLESHOOTING || List of available IDs: " + str(ITEMS.keys()))
+            print('INFO || ItemView.__init__() item text = ' + ITEMS[itemID].name)
 
             self.dicon.source = ITEMS[self.itemID].icon
             self.dname.text = ITEMS[self.itemID].name
 
-            view.add_widget(self.dicon)
-            view.add_widget(self.dname)
+
+            self.view.add_widget(self.dBG)
+            self.view.add_widget(self.dicon)
+            self.view.add_widget(self.dname)
+
+            print('BINDING...\ntext_size = ' + str(self.dname.text_size) + '\nsize = ' + str(self.dname.size))
+
+            self.dname.text_size[0] = IV_NAME.hw * IV_NAME.parentW / scale
 
             self.add_widget(self.view)
 
@@ -40,3 +49,6 @@ class ItemView(Button):
 
         except Exception as ex:
             print(str(ex))
+
+    def on_press(self):
+        print('button named ' + str(self.itemID) + ' was pressed!')
